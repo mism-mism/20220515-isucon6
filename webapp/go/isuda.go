f10a58eb8af5a1670f075aa7698849a657f90a90
masami <msihima0304@gmail.com>
2022-05-15 15:01:49 +0900

    feat: 🎸 正規表現重いぞ問題の解消

--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -325,18 +325,19 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 	for _, entry := range entries {
 		keywords = append(keywords, regexp.QuoteMeta(entry.Keyword))
 	}
-	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
-	kw2sha := make(map[string]string)
-	content = re.ReplaceAllStringFunc(content, func(kw string) string {
-		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		return kw2sha[kw]
-	})
+	
+	kw2sha := make(map[string]string, len(keywords))
+	for _, kw := range keywords {
+		if strings.Contains(content, kw) {
+			kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+			content = strings.Replace(content, kw, kw2sha[kw], -1)
+		}
+	}
 	content = html.EscapeString(content)
 	for kw, hash := range kw2sha {
 		u, err := r.URL.Parse(baseUrl.String() + "/keyword/" + pathURIEscape(kw))
 		panicIf(err)
-		link := fmt.Sprintf("<a href=\"%s\">%s</a>", u, html.EscapeString(kw))
-		content = strings.Replace(content, hash, link, -1)
+		content = strings.Replace(content, hash, fmt.Sprintf("<a href=\"%s\">%s</a>", u, html.EscapeString(kw)), -1)
 	}
 	return strings.Replace(content, "\n", "<br />\n", -1)
 }
