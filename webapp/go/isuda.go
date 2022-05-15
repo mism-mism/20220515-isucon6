3cb1cd4059b6af53da1c3d5136dc95af32495d6e
masami <msihima0304@gmail.com>
2022-05-15 17:00:04 +0900

    feat: 🎸 start postをなをした

--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -414,17 +414,10 @@ func starsHandler(w http.ResponseWriter, r *http.Request) {
 
 func starsPostHandler(w http.ResponseWriter, r *http.Request) {
 	keyword := r.FormValue("keyword")
-
-	origin := os.Getenv("ISUDA_ORIGIN")
-	if origin == "" {
-		origin = "http://localhost:5000"
-	}
-	u, err := r.URL.Parse(fmt.Sprintf("%s/keyword/%s", origin, pathURIEscape(keyword)))
-	panicIf(err)
-	resp, err := http.Get(u.String())
-	panicIf(err)
-	defer resp.Body.Close()
-	if resp.StatusCode >= 400 {
+	row := db.QueryRow(`SELECT * FROM entry WHERE keyword = ?`, keyword)
+	e := Entry{}
+	err := row.Scan(&e.ID, &e.AuthorID, &e.Keyword, &e.Description, &e.UpdatedAt, &e.CreatedAt)
+	if err == sql.ErrNoRows {
 		notFound(w)
 		return
 	}
